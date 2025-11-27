f1bd4736ccdfab01e0d96b5679ec5c5a739de08a
ghazylilhaq <ghazylilhaq@gmail.com>
2025-11-27 18:38:55 +0700

    Fix chart.tsx: remove extra argument from formatter call

--- frontend/components/ui/chart.tsx
+++ frontend/components/ui/chart.tsx
@@ -220,7 +220,7 @@ function ChartTooltipContent({
               )}
             >
               {formatter && item?.value !== undefined && item.name ? (
-                formatter(item.value, item.name, item, index, item.payload)
+                formatter(item.value, item.name, item, index)
               ) : (
                 <>
                   {itemConfig?.icon ? (
